714439787e3d4c775c6b53ff23ad03e41bbaa5e1
Emberstorm <A02391309@usu.edu>
2026-02-09 17:23:50 -0700

    Fixed up app.jsx and game.jsx to make my website actually load now. My CSS is a little messed up but we'll figure that out

--- src/app.jsx
+++ src/app.jsx
@@ -3,40 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
-import { Play } from './game/game';
-import { Scores } from './leaderboard/leaderboard';
-import { About } from './instructions/instructions';
+import { Game } from './game/game';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { Instructions } from './instructions/instructions';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <body class="text-center" style="background-color: #1b7dde;"> 
+      <div className="text-center" style={{ backgroundColor: '#1b7dde' }}>
         <header>
-            <h1>Catch The Fish</h1>
-            <nav>
-                <ul>
-                    <li><NavLink to="index">Home</NavLink></li>
-                    <li><NavLink to="game">Game</NavLink></li>
-                    <li><NavLink to="instructions">Instructions</NavLink></li>
-                    <li><NavLink to="leaderboard">Leaderboard</NavLink></li>
-                </ul>
-            </nav>
+          <h1>Catch The Fish</h1>
+          <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/game">Game</NavLink></li>
+              <li><NavLink to="/instructions">Instructions</NavLink></li>
+              <li><NavLink to="/leaderboard">Leaderboard</NavLink></li>
+            </ul>
+          </nav>
         </header>
 
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/instructions" element={<Instructions />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
         <footer>
-            <p> Author name: Ryan Isaacson</p>
-            <a href="https://github.com/ryan-isaacson/CS-260-repo.git">Github</a>
+          <p> Author name: Ryan Isaacson</p>
+          <a href="https://github.com/ryan-isaacson/CS-260-repo.git">Github</a>
         </footer>
-    </body>
-
-    <Routes>
-      <Route path='/' element={<Login />} exact />
-      <Route path='/game' element={<Game />} />
-      <Route path='/leaderboard' element={<Leaderboard />} />
-      <Route path='/instructions' element={<Instructions />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-
+      </div>
     </BrowserRouter>
 
   )
--- src/game/game.jsx
+++ src/game/game.jsx
@@ -15,34 +15,34 @@ export function Game() {
         </div>
         <button className="start-button">Start Game</button>
         <div className="fish-layer">
-            <button className="fish-button" style="--x: 10; --y: 20;">
+            <button className="fish-button" style={{ "--x": 10, "--y": 20 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
             </button>
-            <button className="fish-button" style="--x: 55; --y: 25;">
+            <button className="fish-button" style={{ "--x": 55, "--y": 25 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
                 </button>
-            <button className="fish-button" style="--x: 85; --y: 15;">
+            <button className="fish-button" style={{ "--x": 85, "--y": 15 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
             </button>
-            <button className="fish-button" style="--x: 20; --y: 60;">
+            <button className="fish-button" style={{ "--x": 20, "--y": 60 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
             </button>
-            <button className="fish-button" style="--x: 45; --y: 90;">
+            <button className="fish-button" style={{ "--x": 45, "--y": 90 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
             </button>
-            <button className="fish-button" style="--x: 75; --y: 70;">
+            <button className="fish-button" style={{ "--x": 75, "--y": 70 }}>
                 <svg width="75" height="75">
-                    <text x="-5" y="70" font-size="80">🐟</text>
+                    <text x="-5" y="70" fontSize="80">🐟</text>
                 </svg>
             </button>
         </div>
