3bedd80b79c8b1e9c331fa92e0b6ede65af01d74
Emberstorm <A02391309@usu.edu>
2026-02-25 21:32:14 -0700

    I don't know what this is, but I changed the deploy script in class a while ago

--- deployReact.sh
+++ deployReact.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+Deployment script
+Now that we are using Vite to bundle our code we need a different deployment script. Delete the deployFiles.sh script and create a new file named deployReact.sh with the following contents.
 
 while getopts k:h:s: flag
 do
@@ -11,8 +12,32 @@ done
 
 if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployReact.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname with $key\n"
+printf "\n----> Deploying React bundle $service to $hostname with $key\n"
+
+# Step 1
+printf "\n----> Build the distribution package\n"
+rm -rf build
+mkdir build
+npm install # make sure vite is installed so that we can bundle
+npm run build # build the React front end
+cp -rf dist/* build # move the React front end to the target distribution
+
+# Step 2
+printf "\n----> Clearing out previous distribution on the target\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+rm -rf services/${service}/public
+mkdir -p services/${service}/public
+ENDSSH
+
+# Step 3
+printf "\n----> Copy the distribution package to the target\n"
+scp -r -i "$key" build/* ubuntu@$hostname:services/$service/public
+
+# Step 5
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf build
+rm -rf dist
